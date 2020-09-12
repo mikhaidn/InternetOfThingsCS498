5f3a5b49d1a7f38558c201f206c76a393d5b152f
Dan Mikhail <danmikhail+github@gmail.com>
2020-09-12 17:04:13 -0500

    fixes

--- scripts/checkBattery.sh
+++ scripts/checkBattery.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-voltsRead = `picar-4wd power-read | tail -n 1 |grep -oE '[^ ]+$'`
+voltsRead=`picar-4wd power-read | tail -n 1 |grep -oE '[^ ]+$'`
 # Suggested supply voltage: 6V-8.5V
 # My theoretical max: 3.7V x 2 = 7.4V
-max=7.4V
-min=6.0V
-voltsNum = ${voltsRead::-1}
-maxNum=${max::-1}
-minNum=${min::-1}
-totalBattery=`awk -v vo=$voltsNum 'BEGIN { print vo/7.6 }'`
-deltaBattery=`awk -v vo=$voltsNum 'BEGIN { print ((vo-6)/1.4)*100 }'`
+# Working range: 6V-7.4V
+curV=${voltsRead::-1}
+
+# total = currentVoltage / theoreticalMaxV
+totalBattery=`awk -v vo=$curV 'BEGIN { print vo/7.4 }'`
+
+# DeltaV = 7.4 - 6 = 1.4
+# DeltaRemaining = curV/DeltaV
+deltaBattery=`awk -v vo=$curV 'BEGIN { print ((vo-6)/1.4)*100 }'`
 echo "Total Battery: $totalBattery"
 echo "Delta Left: ${deltaBattery}%"
