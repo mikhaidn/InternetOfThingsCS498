413d722521c642b70845fd9df875ae2bf2a53812
Daniel Mikhail <daniel.mikhail@oath.com>
2020-09-12 10:35:58 -0500

    fixName

--- scripts/takephoto.sh
+++ scripts/takephoto.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+NAME=""
+parsed_options=$(
+  getopt -n "$0" -o n -- "$@"
+) || exit
+eval "set -- $parsed_options"
+while [ "$#" -gt 0 ]; do
+  case $1 in
+    (-n) NAME = ${2}_; shift 2;;
+    (--) shift; break;;
+    (*) exit 1 # should never be reached.
+  esac
+done
+echo "Now, the arguments are $*"
+
+
 DATE=$(date +"%Y-%m-%d_%H%M")
+FILE_NAME=${NAME}${DATE}
 REPO="/home/pi/git/mikhaidn/InternetOfThingsCS498/Labs/Lab1-Self-Driving-Car-Base/images"
-raspistill "$@" -vf -hf -o ${REPO}/$DATE.jpg 
+raspistill "$@" -vf -hf -o ${REPO}/${FILE_NAME}.jpg 
